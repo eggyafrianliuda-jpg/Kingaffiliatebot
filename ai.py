0012361606807080b68fd13cebbacf2bca7d53f9
eggyafrianliuda-jpg <eggyafrianliuda@gmail.com>
2026-07-03 22:02:26 +0700

    Create ai.py

---
+++ ai.py
@@ -0,0 +1,10 @@
+import google.generativeai as genai
+from config import GEMINI_API_KEY
+
+genai.configure(api_key=GEMINI_API_KEY)
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+def generate(prompt):
+    response = model.generate_content(prompt)
+    return response.text
