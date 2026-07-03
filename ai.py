371d5e57148405ad661633085f4b6a687835b3fd
eggyafrianliuda-jpg <eggyafrianliuda@gmail.com>
2026-07-03 22:54:46 +0700

    Update ai.py

--- ai.py
+++ ai.py
@@ -3,11 +3,4 @@ from config import GEMINI_API_KEY
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-2.0-flash")
-
-def generate(prompt):
-    try:
-        response = model.generate_content(prompt)
-        return response.text
-    except Exception as e:
-        return str(e)
+model = genai.GenerativeModel("gemini-2.5-flash")
